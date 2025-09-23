8b2615623c04acf48f3a6842f62a0f99cf4ec9d5
Valentin <valentin.godard2006@gmail.com>
2025-09-23 14:34:15 +0200

    match

--- match.php
+++ match.php
@@ -2,18 +2,51 @@
 
 class equipe {
 
-    private string $nom;
+    private string $equipe1Score;
 
-    public function __construct(string $nom) {
-        $this->nom = $nom;
+    private string $equipe2Score;
+
+    private DateTime $date;
+
+    private string $city;
+
+    public function __construct(string $equipe1Score, string $equipe2Score, DateTime $date, string $city) {
+
+        $this->equipe1Score = $equipe1Score;
+        $this->equipe2Score = $equipe2Score;
+        $this->date = $date;
+        $this->city = $city;
     }
 
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
+    public function getEquipe1Score(): string {
+        return $this->equipe1Score;
     }
 
-    public function getNom(){
-        $this->nom;
+    public function setEquipe1Score(string $equipe1Score): void {
+        $this->equipe1Score = $equipe1Score;
+    }
+
+    public function getEquipe2Score(): string {
+        return $this->equipe2Score;
+    }
+
+    public function setEquipe2Score(string $equipe2Score): void {
+        $this->equipe2Score = $equipe2Score;
+    }
+
+    public function getDate(): DateTime {
+        return $this->date;
+    }
+
+    public function setDate(DateTime $date): void {
+        $this->date = $date;
+    }
+
+    public function getCity(): string {
+        return $this->city;
+    }
+
+    public function setCity(string $city): void {
+        $this->city = $city;
     }
 }
