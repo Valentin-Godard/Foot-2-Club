6a8c3f886e24c42777e24de10f7e4c10d5882699
Redwane Kouadria <redwanekouadria@gmail.com>
2025-10-06 09:49:17 +0200

    class database

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,52 @@
 <?php
-$host = "localhost";
-$dbname = "footclub";
-$username = "root";
-$password = "";
+namespace App;
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+use PDO;
+use PDOException;
+
+
+class Database
+{
+    private static $aa = null;
+    private $connection;
+
+    private function __construct()
+    {
+        $host = "localhost";
+        $dbname = "footclub";
+        $username = "root";
+        $password = "";
+
+        try {
+            $this->connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    { // Singleton : Empêche la création de plusieurs instances de la base de données, si instance déjà créée, on la réutilise
+        if (self::$aa === null) {
+            self::$aa = new Database();
+        }
+        return self::$aa;
+    }
+    
+    public function getConnection()
+    {
+        return $this->connection;
+    }
 }
+
+$db = Database::getInstance();
+$connection = $db->getConnection();
+
+var_dump($connection);
+?>
+
+<br>
+<br>
+
+<?php
+var_dump($db);
